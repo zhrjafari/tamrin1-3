96be460066a5633395c3d32a357cd534d6549b04
zahrajafari <jafari_zhr@yahoo.com>
2023-04-15 22:27:45 +0330

    Add project files.

---
+++ tamrin1(3)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("enter num1 & num2");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Calc(num1, num2);
+        }//end main
+
+        private static void Calc(double num1, double num2)
+        {
+            double Result = Math.Sqrt(Math.Abs(num1)+Math.Pow(num2,2));
+            Console.WriteLine(Result);
+        }
+    }
+}
